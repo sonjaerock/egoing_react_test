1b52ab7654cbca8e9781d36c12b9a04493ca2078
sonejaerock <kadamon2007@afreecatv.com>
2021-03-25 21:25:34 +0900

    'test'

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content(props) {
+    return (
+        <article>
+            <h2>{ props.title }</h2>
+            { props.desc }
+        </article>
+    );
+}
+
+export default Content;
